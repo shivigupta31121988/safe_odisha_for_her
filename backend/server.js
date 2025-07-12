c760ed01adc882d3a96dff6e8fdb79b408a17527
Shivi Gupta <shivigupta31121988@gmail.com>
2025-07-12 18:32:31 +0530

    fix, moved .env file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({override: false}); // Load environment variables from .env file
 
 const app = express();
 app.use(express.json());
